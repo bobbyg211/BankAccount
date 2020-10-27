6c02d86d0d7b825f247b27074e6d09e646d11eff
bobbyg211 <robertdg96@gmail.com>
2020-10-26 20:15:36 -0500

    Unit tests base

--- main.js
+++ main.js
@@ -43,61 +43,35 @@ if( typeof describe == 'function'){
 
   describe("constructor test", function(){
     it("should handle simple constructor", function(){
-      let mazda = new Car("1234", 31, 13);
-      assert.equal(mazda.id, "1234");
-      assert.equal(mazda.mpg, 31);
-      assert.equal(mazda.tankSize, 13);
-      assert.equal(mazda.currentFuel, 0);
-      assert.equal(mazda.odometer, 0);
+
     })
   })
 
-  describe("adding fuel", function(){
+  describe("return account balance", function(){
     it("partially fill the tank", function(){
-      let mazda = new Car("1234", 31, 13);
-      mazda.fillUp(1);
-      assert.equal(mazda.currentFuel, 1);
-      mazda.fillUp(4);
-      assert.equal(mazda.currentFuel, 5);
-    })
 
-    it("should not overfill", function(){
-      let mazda = new Car("1234", 31, 13);
-      mazda.fillUp(15);
-      assert.equal(mazda.currentFuel, 0);
-    })
-    
-    it("should not allow negative fuel", function(){
-      let mazda = new Car("1234", 31, 13);
-      mazda.fillUp(10);
-      mazda.fillUp(-3);
-      assert.equal(mazda.currentFuel, 10);
     })
   })
 
-  describe("distance to empty", function(){
-    it("return total miles you can travel", function(){
-      let mazda = new Car("1234", 31, 13);
-      mazda.fillUp(10);
-      assert.equal(mazda.distanceToEmpty(), 310);
+  describe("deposit money to account", function(){
+    it("balance increases after deposit", function(){
+
+    })
+    it("dont allow for negative number", function(){
+
     })
   })
 
-  describe("driving a certain distance", function(){
-    it("should return total distance traveled", function(){
+  describe("charge money to account", function(){
+    it("cannot overcharge", function(){
       let mazda = new Car("1234", 31, 13);
       mazda.fillUp(10);
       assert.equal(mazda.drive(50), true);
     })
-    it("should not allow driving farther than possible", function(){
+    it("allow for positive or negative numbers", function(){
       let mazda = new Car("1234", 31, 13);
       mazda.fillUp(1);
       assert.equal(mazda.drive(50), false);
-    })
-    it("should not allow driving backwards", function(){
-      let mazda = new Car("1234", 31, 13);
-      mazda.fillUp(10);
-      assert.equal(mazda.drive(-20), false);
     })
   })
 }
