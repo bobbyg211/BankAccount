6232f513ccc5dca57e15e7e8dfbb9f329b532d96
bobbyg211 <robertdg96@gmail.com>
2020-10-28 17:00:27 -0500

    Unit tests

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { appendFileSync } = require('fs');
+
 // Bank Account Class
 class BankAccount {
   constructor(accountNumber, owner) {
@@ -15,13 +17,21 @@ class BankAccount {
   }
 
   deposit(amt) {
-    let newDeposit = new Transaction(amt, "Deposit");
-    this.transactions.push(newDeposit);
+    if (amt < 0) {
+      return false;
+    } else {
+      let newDeposit = new Transaction(amt, "Deposit");
+      this.transactions.push(newDeposit);
+    }
   }
 
   charge(amt, payee) {
-    let newCharge = new Transaction(-Math.abs(amt), payee);
-    this.transactions.push(newCharge);
+    if (this.balance() >= amt) {
+      let newCharge = new Transaction(-Math.abs(amt), payee);
+      this.transactions.push(newCharge);
+    } else {
+      return false;
+    }
   }
 }
 
@@ -34,46 +44,46 @@ class Transaction {
   }
 }
 
-const myAccount = new BankAccount("12345", "Robert");
-myAccount.deposit(35);
-myAccount.charge(15, "Internet");
-console.log(myAccount);                    
-
-
-console.log("My account balance is:", myAccount.balance());
-
 // Unit Tests
 
 if( typeof describe == 'function'){
   const assert = require('assert');
 
-  describe("constructor test", function(){
-    it("should handle simple constructor", function(){
-
-    })
-  })
-
   describe("return account balance", function(){
-    it("partially fill the tank", function(){
-
+    it("add up transactions", function(){
+      const myAccount = new BankAccount("12345", "Robert");
+      myAccount.deposit(35);
+      myAccount.charge(15, "Internet");    
+      assert.equal(myAccount.balance(), 20);
     })
   })
 
   describe("deposit money to account", function(){
     it("balance increases after deposit", function(){
-
+      const myAccount = new BankAccount("12345", "Robert");
+      myAccount.deposit(50);
+      assert.equal(myAccount.balance(), 50);
     })
     it("dont allow for negative number", function(){
-
+      const myAccount = new BankAccount("12345", "Robert");
+      assert.equal(myAccount.deposit(-50), false);
     })
   })
 
   describe("charge money to account", function(){
     it("cannot overcharge", function(){
-
+      const myAccount = new BankAccount("12345", "Robert");
+      myAccount.deposit(30);
+      assert.equal(myAccount.charge(60), false);
+      assert.equal(myAccount.balance(), 30);
     })
     it("allow for positive or negative numbers", function(){
-
+      const myAccount = new BankAccount("12345", "Robert");
+      myAccount.deposit(50);
+      myAccount.charge(15);
+      assert.equal(myAccount.balance(), 35);
+      myAccount.charge(-15);
+      assert.equal(myAccount.balance(), 20);
     })
   })
 }
